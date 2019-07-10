0778b9179885c07ea82295a38ad51f3068f83c2f
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-07-10 09:56:28 +0630

    test

---
+++ app/src/main/java/com/hms/collapseexample/ScrollingActivity.kt
@@ -0,0 +1,24 @@
+package com.hms.collapseexample
+
+import android.os.Bundle
+import android.support.design.widget.CollapsingToolbarLayout
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_scrolling.*
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+
+//        val toolbar:Toolbar =findViewById(R.id.toolbar)
+//        toolbar.title = "KBZ Pay"
+
+//        val collapsingToolbarLayout: CollapsingToolbarLayout = findViewById(R.id.collapsingToolbar)
+//        collapsingToolbarLayout.title = "KBZ Pay"
+    }
+}
